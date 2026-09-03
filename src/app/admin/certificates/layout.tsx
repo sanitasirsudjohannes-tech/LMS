3100079899e60907ca4ad7f94ace916eaa3b24c1
sanitasirsudjohannes-tech <sanitasirsudjohannes@gmail.com>
2026-09-03 16:24:46 +0800

    refactor: remove duplicate certificate tabs layout

--- src/app/admin/certificates/layout.tsx
+++
@@ -1,5 +0,0 @@
-import CertificateAdminTabs from '@/components/CertificateAdminTabs';
-
-export default function CertificatesLayout({ children }: { children: React.ReactNode }) {
-  return <div className="space-y-5"><CertificateAdminTabs />{children}</div>;
-}
